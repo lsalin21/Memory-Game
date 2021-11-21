29e1f40a1e53d4ed739aca9eae0e16292200aeab
lsalin <lsalin21@gmail.com>
2021-11-21 02:33:08 -0600

    Fixed bugs in js file

--- script.js
+++ script.js
@@ -4,51 +4,51 @@ document.addEventListener('DOMContentLoaded',() => {
     const cardArray = [
         {
             name: 'wolf',
-            img: 'images/Wolf.jpg'
+            img: 'images/wolf.jpg'
         },
         {
             name: 'bison',
-            img: 'images/Wolf.jpg'
+            img: 'images/bison.jpg'
         },
         {
             name: 'bear',
-            img: 'images/Bear.jpg'
+            img: 'images/bear.jpg'
         },
         {
             name: 'eagle',
-            img: 'images/Bear.jpg'
+            img: 'images/eagle.jpg'
         },
         {
             name: 'owl',
-            img: 'images/Bison.jpg'
+            img: 'images/owl.jpg'
         },
         {
             name: 'squirrel',
-            img: 'images/Bison.jpg'
+            img: 'images/squirrel.jpg'
         },
         {
             name: 'wolf',
-            img: 'images/Eagle.jpg'
+            img: 'images/wolf.jpg'
         },
         {
             name: 'bison',
-            img: 'images/Eagles.jpg'
+            img: 'images/bison.jpg'
         },
         {
             name: 'bear',
-            img: 'images/Owl.jpg'
+            img: 'images/bear.jpg'
         },
         {
             name: 'eagle',
-            img: 'images/Owl.jpg'
+            img: 'images/eagle.jpg'
         },
         {
             name: 'owl',
-            img: 'images/Squirrel.jpg'
+            img: 'images/owl.jpg'
         },
         {
             name: 'squirrel',
-            img: 'images/Squirrel.jpg'
+            img: 'images/squirrel.jpg'
         }
     ]
 
@@ -63,9 +63,9 @@ let cardsWon = []
 function createBoard(){
     for(let i = 0; i < cardArray.length; i++){
         const card = document.createElement('img')
-        card.setAttribute('src', 'images/Day.jpg')
+        card.setAttribute('src', 'images/day.jpg')
         card.setAttribute('date-id', i)
-        card.addEventListener('click', flipcard)
+        card.addEventListener('click', flipCard)
         grid.appendChild(card)
     }
 }
@@ -75,19 +75,19 @@ function checkForMatch(){
     const optionOneId = cardsChosenId[0]
     const optionTwoId = cardsChosenId[1]
     if(optionOneId == optionTwoId){
-        cards[optionOneId].setAttribute('src', 'images/Day.jpg')
-        cards[optionTwoId].setAttribute('src', 'images/Day.jpg')
+        cards[optionOneId].setAttribute('src', 'images/day.jpg')
+        cards[optionTwoId].setAttribute('src', 'images/day.jpg')
         alert('You have clicked the same image')
     }else if(cardsChosen[0] === cardsChosen[1]){
         alert('You found a match')
-        cards[optionOneId].setAttribute('src', 'images/Night.jpg')
-        cards[optionTwoId].setAttribute('src', 'images/Night.jpg')
-        cards[optionOneId].removeEventListener('click', flipcard)
-        cards[optionTwoId].removeEventListener('click', flipcard)
+        cards[optionOneId].setAttribute('src', 'images/night.jpg')
+        cards[optionTwoId].setAttribute('src', 'images/night.jpg')
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
         cardsWon.push(cardsChosen)
     }else{
-        cards[optionOneId].setAttribute('src', 'images/Day.jpg')
-        cards[optionTwoId].setAttribute('src', 'images/Day.jpg')
+        cards[optionOneId].setAttribute('src', 'images/day.jpg')
+        cards[optionTwoId].setAttribute('src', 'images/day.jpg')
         alert('Sorry, try again')
     }
     cardsChosen = []
@@ -98,12 +98,12 @@ function checkForMatch(){
     }
 }
 
-function flipcard(){
+function flipCard(){
     let cardId = this.getAttribute('data-id')
     cardsChosen.push(cardArray[cardId].name)
     cardsChosenId.push(cardId)
-    this.setAttribute('src', cardArray(cardId).img)
-    if(cardsChosen.length === 2){
+    this.setAttribute('src', cardArray[cardId].img)
+    if(cardsChosen.length ===2){
         setTimeout(checkForMatch, 500)
     }
 }
