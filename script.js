3ede0f8d7ebefca2984a044b4ba3c04891360a66
lsalin <lsalin21@gmail.com>
2021-11-21 00:26:05 -0600

    Added new folder

---
+++ script.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded',() => {
+
+    //card options
+    const cardArray = [
+        {
+            name: 'wolf',
+            img: 'images/Wolf.jpg'
+        },
+        {
+            name: 'wolf',
+            img: 'images/Wolf.jpg'
+        },
+        {
+            name: 'bear',
+            img: 'images/Bear.jpg'
+        },
+        {
+            name: 'bear',
+            img: 'images/Bear.jpg'
+        },
+        {
+            name: 'bison',
+            img: 'images/Bison.jpg'
+        },
+        {
+            name: 'bison',
+            img: 'images/Bison.jpg'
+        },
+        {
+            name: 'eagle',
+            img: 'images/Eagle.jpg'
+        },
+        {
+            name: 'eagle',
+            img: 'images/Eagles.jpg'
+        },
+        {
+            name: 'owl',
+            img: 'images/Owl.jpg'
+        },
+        {
+            name: 'owl',
+            img: 'images/Owl.jpg'
+        },
+        {
+            name: 'squirrel',
+            img: 'images/Squirrel.jpg'
+        },
+        {
+            name: 'squirrel',
+            img: 'images/Squirrel.jpg'
+        },
+    ]
+})
