f4a1b1de57d9411470f2c2f1dbd39b0d27cfa5e3
lsalin <lsalin21@gmail.com>
2021-11-21 02:42:23 -0600

    Fixed line 103 bug on script.js

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ function createBoard(){
     for(let i = 0; i < cardArray.length; i++){
         const card = document.createElement('img')
         card.setAttribute('src', 'images/day.jpg')
-        card.setAttribute('date-id', i)
+        card.setAttribute('data-id', i)
         card.addEventListener('click', flipCard)
         grid.appendChild(card)
     }
